d8f210b7219ea417cec74aad9d7d58eb01d7e3d2
thedavidmeister <thedavidmeister@gmail.com>
2024-04-08 20:04:03 +0400

    fix interface version

--- src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
+++ src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.18;
 import {IOrderBookV4OrderTaker} from "./IOrderBookV4OrderTaker.sol";
 import {TakeOrdersConfigV3} from "./IOrderBookV4.sol";
 
-interface IOrderBookV3ArbOrderTaker is IOrderBookV4OrderTaker {
+interface IOrderBookV4ArbOrderTaker is IOrderBookV4OrderTaker {
     function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput) external payable;
 }
