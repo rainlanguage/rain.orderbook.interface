24da531619b1d596b227bfd4b1c7705dbaf1538e
thedavidmeister <thedavidmeister@gmail.com>
2024-04-08 16:54:22 +0400

    verion interface methods

--- src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
+++ src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
@@ -5,5 +5,5 @@ import {IOrderBookV4OrderTaker} from "./IOrderBookV4OrderTaker.sol";
 import {TakeOrdersConfigV3} from "./IOrderBookV4.sol";
 
 interface IOrderBookV3ArbOrderTaker is IOrderBookV4OrderTaker {
-    function arb(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput) external payable;
+    function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput) external payable;
 }
