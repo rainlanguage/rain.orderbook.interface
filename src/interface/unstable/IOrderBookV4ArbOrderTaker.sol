9aed474c40e2027c64c6bcd1731215a0f184891e
thedavidmeister <thedavidmeister@gmail.com>
2024-04-13 12:17:28 +0400

    fmt

--- src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
+++ src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
@@ -6,5 +6,7 @@ import {TakeOrdersConfigV3} from "./IOrderBookV4.sol";
 import {EvaluableV3} from "./IOrderBookV4.sol";
 
 interface IOrderBookV4ArbOrderTaker is IOrderBookV4OrderTaker {
-    function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput, EvaluableV3 calldata evaluable) external payable;
+    function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput, EvaluableV3 calldata evaluable)
+        external
+        payable;
 }
