2ed622eb256369419be08d1c130e8a9a16ae4dda
thedavidmeister <thedavidmeister@gmail.com>
2024-04-13 12:08:51 +0400

    add evaluable to v4 arb order taker

--- src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
+++ src/interface/unstable/IOrderBookV4ArbOrderTaker.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.18;
 
 import {IOrderBookV4OrderTaker} from "./IOrderBookV4OrderTaker.sol";
 import {TakeOrdersConfigV3} from "./IOrderBookV4.sol";
+import {EvaluableV3} from "./IOrderBookV4.sol";
 
 interface IOrderBookV4ArbOrderTaker is IOrderBookV4OrderTaker {
-    function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput) external payable;
+    function arb2(TakeOrdersConfigV3 calldata takeOrders, uint256 minimumSenderOutput, EvaluableV3 calldata evaluable) external payable;
 }
